a5f75b7e656b98aacc40aa72cac2c4717cd7b748
Sergio-Vazquez <svazquezpelaez@gmail.com>
2015-03-20 00:14:18 +0100

    Choose dataset.

---
+++ Analisis_estadistico.R
@@ -0,0 +1,197 @@
+############################ ANALISIS ESTADISTICO - MODULO 10 DATA SCIENCE - UTAD ###############################
+
+# Para esta práctica, he elegido el dataset "diamonds" que contendrá el precio (entre otras variables
+# interesantes) de unos 54.000 diamantes.
+
+# Parece un dataset tipo bastante interesante con el que poder realizar distintos estudios estadísticos.
+
+# El objetivo será por lo tanto, realizar distintos tipos de análisis estadísticos de sus variables para intentar
+# averiguar algún tipo de comportamiento oculto aparentemente en los datos.
+
+# Los diferentes indicadores presentes en el dataset "diamonds" son los siguientes:
+
+# price: Precio en dolares americanos ($326–$18,823)
+# carat: peso del diamante (0.2–5.01)
+# cut: calidad del corte (Fair, Good, Very Good, Premium, Ideal)
+# colour: color del diamante (desde D el mejor hasta J el peor)
+# clarity: mide como de claro es el diamante (desde el peor I1, SI2, SI1, VS2, VS1, VVS2, VVS1, hasta el mejor IF)
+# x: longitud en mm (0–10.74)
+# y: ancho en  mm (0–58.9)
+# z: profundidad en mm (0–31.8)
+# depth: porcentaje total de profundidad (total depth percentage = z / mean(x, y) = 2 * z / (x + y) (43–79))
+# table: anchura de la parte superior de diamante con relación al punto más ancho (43-95)
+
+################################# TRATAMIENTO DE LOS DATOS INICIALES ############################################
+
+# 0- Instalacion de librerias.
+
+library(ggplot2) # necesaria para cargar el dataset diamonds
+library(stats) # stadistica
+library(prettyR)
+library(manipulate) # para dibujar graficos
+library(rgl) #visualizaciones 3d
+library(e1071)
+library(modeest) # contiene estimadores en el modo unimodal y alguno multimodal
+library(quantmod)
+library(dplyr)
+library(nortest)
+
+# 1- Obtenemos el dataset diamonds.
+data(diamonds)
+
+# 2- Analizamos de forma rapida los datos presentes en el dataset.
+summary(diamonds) # obtenemos un resumen de los datos
+
+# 3 - Modificacion de datos
+# Debido a que hay 3 variables muy importantes y que no son numericas, deberemos de realizar una reclasificacion para incluirlas
+# El objetivo sera cambiar las variables cut, colour y clarity para pasarlas a tipo "double".
+
+# Creamos un nuevo data frame que modificaremos 
+df.diamonds <- diamonds
+
+
+# cambiamos los valores de la variable cut, y las renombramos del 1 al 5, de mejor(Ideal) a peor(Fair):
+
+df.diamonds$cut2 <- as.numeric(df.diamonds$cut)
+
+df.diamonds$cut2[df.diamonds$cut == "Ideal"] <- 1
+df.diamonds$cut2[df.diamonds$cut == "Premium"] <- 2
+df.diamonds$cut2[df.diamonds$cut == "Very Good"] <- 3
+df.diamonds$cut2[df.diamonds$cut == "Good"] <- 4
+df.diamonds$cut2[df.diamonds$cut == "Fair"] <- 5
+
+# hacemos lo mismo para la variable color:
+
+df.diamonds$color2 <- as.numeric(df.diamonds$color)
+
+df.diamonds$color2[df.diamonds$color == "D"] <- 1
+df.diamonds$color2[df.diamonds$color == "E"] <- 2
+df.diamonds$color2[df.diamonds$color == "F"] <- 3
+df.diamonds$color2[df.diamonds$color == "G"] <- 4
+df.diamonds$color2[df.diamonds$color == "H"] <- 5
+df.diamonds$color2[df.diamonds$color == "I"] <- 6
+df.diamonds$color2[df.diamonds$color == "J"] <- 7
+
+
+# hacemos lo mismo para la variable clarity:
+
+df.diamonds$clarity2 <- as.numeric(df.diamonds$clarity)
+
+df.diamonds$clarity2[df.diamonds$clarity == "IF"] <- 1
+df.diamonds$clarity2[df.diamonds$clarity == "VVS1"] <- 2
+df.diamonds$clarity2[df.diamonds$clarity == "VVS2"] <- 3
+df.diamonds$clarity2[df.diamonds$clarity == "VS1"] <- 4
+df.diamonds$clarity2[df.diamonds$clarity == "VS2"] <- 5
+df.diamonds$clarity2[df.diamonds$clarity == "SI1"] <- 6
+df.diamonds$clarity2[df.diamonds$clarity == "SI2"] <- 7
+df.diamonds$clarity2[df.diamonds$clarity == "I1"] <- 8
+
+
+# Una vez tenemos todas las variables como numericas, creo un data frame con las columnas que me interesan:
+df.estudio <- df.diamonds[, c(1,5:13)]
+class(df.estudio) # es un dataframe
+
+# Podremos realizar otro "summary" para ver cómo quedarian los datos con estas modificaciones:
+summary(df.estudio)
+
+
+##################################### INFORMACION DATAFRAME ####################################################
+
+# Obtenemos sus dimensiones:
+dimension<-dim(df.estudio) # Es un dataframe de 53.940 x 10
+nobs<-dimension[1] 
+nvar<-dimension[2]
+nobs # Tenemos 53.940 observaciones
+nvar # Hay 10 variables
+
+
+# Realizamos un attach al dataframe, de forma que podamos acceder a sus valores directamente con el nombre
+# de la variable.
+
+attach(df.estudio) # activar
+detach(df.estudio) # desactivar
+
+
+################################### ANALISIS DE VARIABLES #####################################################
+
+######### Variable "carat" - Variable cuantitativa continua 
+
+nobs <- length(carat) # tiene 53.940 observaciones
+
+# Posibles valores vacios
+sum(is.na(carat))  # comprobamos que no presenta valores vacios
+
+# Análisis de frecuencias
+
+## Frecuencia absoluta:
+fabscar<-table(carat)
+fabscar
+
+## Frecuencia relativa:
+frelcar<-fabscar/sum(fabscar)
+frelcar
+
+## Frecuencia absoluta acumulada:
+fabsacumcar<-as.table(cumsum(fabscar))
+fabsacumcar
+
+## Frecuencia relativa acumulada:
+frelacumcar<-as.table(cumsum(frelcar))
+frelacumcar # Comprobamos que la suma de las frecuencia relativas acumuladas es 1.
+
+# Resumen 
+summary(carat) #media: 0.7979, mediana: 0.7
+mfv(carat) # moda 0.3
+quantile(carat) # visualizacio de los cuantiles. Q1 = 0.40, Q3 = 1.04 (la diferencia entre esos cuantiles
+                # es el rango intercuartilico)
+# El rango intercuartilico es de 0.64. Calculamos los dos bigotes.
+# Bigote superior: 1.04 + 1.5*0.64 = 2
+# Bigote inferior: 0.40 - 1.5*0.64 = 0.08
+
+var(carat)   # varianza de 0.2246867
+sd(carat)    # desviacion tipica 0.4740112
+
+# Representacion
+pie(frelcar)
+hist(carat)
+boxplot(carat) # existen varios valores extremos
+
+# eliminacion de valores extremos
+max(carat)  # el valor maximo es de 5.01
+carat2<-subset(carat, carat < 2)
+nobscarat2<-length(carat2) # Tiene 51.786 observaciones (53.940 eran las obs. iniciales)
+
+# Nuevo análisis de frecuencias sin valores extremos
+
+## Frecuencia absoluta:
+fabscar2<-table(carat2)
+fabscar2
+
+## Frecuencia relativa:
+frelcar2<-fabscar2/sum(fabscar2)
+frelcar2
+
+## Frecuencia absoluta acumulada:
+fabsacumcar2<-as.table(cumsum(fabscar2))
+fabsacumcar2
+
+## Frecuencia relativa acumulada:
+frelacumcar2<-as.table(cumsum(frelcar2))
+frelacumcar2 # Comprobamos que la suma de las frecuencia relativas acumuladas es 1.
+
+# Resumen
+summary(carat2) #media: 0.7423, mediana: 0.7 , mantiene unos valores similares a antes
+mfv(carat) # moda 0.3, misma moda que antes.
+
+# cuartiles
+quantile(carat2)
+
+# representaci?n
+boxplot(carat2) # se comprueba que no hay valores extremos como antes
+var(carat2)   # 0.1544
+sd(carat2)    # 0.39300
+
+# contraste
+sort(carat)
+lillie.test(carat) #se rechaza la Hipotesis de normalidad
+lillie.test(carat2) #se rechaza la Hipotesis de normalidad
